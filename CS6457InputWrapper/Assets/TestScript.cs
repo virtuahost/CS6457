ae2e5606febf15d826b76016fced2702ce7a2141
Deep Ghosh <deep@th.ru.st>
2015-09-01 11:31:50 -0400

    Modified the script for testing

--- CS6457InputWrapper/Assets/TestScript.cs
+++ CS6457InputWrapper/Assets/TestScript.cs
@@ -1,7 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class TestScript : MonoBehaviour {
+
+    //Button text
+    public Text btnText;
+
+    //Axis Text and Name
+    public Text axisText;
+    public Text axisName;
+
+    //Trigger Text and Name    
+    public Text trgText;
+    public Text trgName;
 
 	// Use this for initialization
 	void Start () {
@@ -10,24 +22,105 @@ public class TestScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        btnText.text = "";
+        trgName.text = "";
+        trgText.text = "";
+        axisName.text = "";
+        axisText.text = "";
+
         if (ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.A))
-            Debug.Log("Button A");
-        Debug.Log("Button B: " + ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.B));
-        Debug.Log("Button Back: " + ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.Back));
-        Debug.Log("Button Left Bumper: " + ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.LeftBumper));
-        Debug.Log("Button Right Bumper: " + ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.RightBumper));
-        Debug.Log("Button Left Stick Click: " + ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.LeftStickClick));
-        Debug.Log("Button Right Stick Click: " + ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.RightStickClick));
-        Debug.Log("Button Start: " + ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.Start));
-        Debug.Log("Button X: " + ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.X));
-        Debug.Log("Button Y: " + ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.Y));
-        Debug.Log("Trigger Left: " + ControlInputWrapper.GetTrigger(ControlInputWrapper.Triggers.LeftTrigger));
-        Debug.Log("Trigger Right: " + ControlInputWrapper.GetTrigger(ControlInputWrapper.Triggers.RightTrigger));
-        Debug.Log("Axis Right Stick Y: " + ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.RightStickY));
-        Debug.Log("Axis Right Stick X: " + ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.RightStickX));
-        Debug.Log("Axis Left Stick Y: " + ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.LeftStickY));
-        Debug.Log("Axis Left Stick X: " + ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.LeftStickX));
-        Debug.Log("Axis Dpad Y: " + ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.DPadY));
-        Debug.Log("Axis Dpad X: " + ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.DPadX));
+            btnText.text = "A";
+        if (ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.B))
+            btnText.text = "B";
+        if (ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.Back))
+            btnText.text = "Back";
+        if (ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.LeftBumper))
+            btnText.text = "Left Bumper";
+        if (ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.RightBumper))
+            btnText.text = "Right Bumper";
+        if (ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.LeftStickClick))
+            btnText.text = "Left Stick Clicked";
+        if (ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.RightStickClick))
+            btnText.text = "Right Stick Click";
+        if (ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.Start))
+            btnText.text = "Start";
+        if (ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.X))
+            btnText.text = "X";
+        if (ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.Y))
+            btnText.text = "Y";
+
+        if (ControlInputWrapper.GetTrigger(ControlInputWrapper.Triggers.LeftTrigger) != 0)
+        {
+            trgName.text = "Left Trigger";
+            trgText.text = ControlInputWrapper.GetTrigger(ControlInputWrapper.Triggers.LeftTrigger).ToString();
+        }
+        if (ControlInputWrapper.GetTrigger(ControlInputWrapper.Triggers.RightTrigger) != 0)
+        {
+            trgName.text = "Right Trigger";
+            trgText.text = ControlInputWrapper.GetTrigger(ControlInputWrapper.Triggers.RightTrigger).ToString();
+        }
+
+        if (ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.RightStickY) > 0)
+        {
+            axisName.text = "Right Stick Down";
+            axisText.text = ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.RightStickY).ToString();
+        }
+        else if (ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.RightStickY) < 0)
+        {
+            axisName.text = "Right Stick Up";
+            axisText.text = ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.RightStickY).ToString();
+        }
+        if (ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.RightStickX) > 0)
+        {
+            axisName.text = "Right Stick right";
+            axisText.text = ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.RightStickX).ToString();
+        }
+        else if (ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.RightStickX) < 0)
+        {
+            axisName.text = "Right Stick left";
+            axisText.text = ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.LeftStickY).ToString();
+        }
+
+        if (ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.LeftStickY) > 0)
+        {
+            axisName.text = "Left Stick Down";
+            axisText.text = ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.LeftStickY).ToString();
+        }
+        else if (ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.LeftStickY) < 0)
+        {
+            axisName.text = "Left Stick Up";
+            axisText.text = ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.LeftStickY).ToString();
+        }
+        if (ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.LeftStickX) > 0)
+        {
+            axisName.text = "Left Stick right";
+            axisText.text = ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.LeftStickX).ToString();
+        }
+        else if (ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.LeftStickX) < 0)
+        {
+            axisName.text = "Left Stick left";
+            axisText.text = ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.LeftStickX).ToString();
+        }
+
+        if (ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.DPadY) > 0)
+        {
+            axisName.text = "Dpad Down";
+            axisText.text = ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.DPadY).ToString();
+        }
+        else if (ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.DPadY) < 0)
+        {
+            axisName.text = "Dpad Up";
+            axisText.text = ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.DPadY).ToString();
+        }
+        if (ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.DPadX) > 0)
+        {
+            axisName.text = "Dpad right";
+            axisText.text = ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.DPadX).ToString();
+        }
+        else if (ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.DPadX) < 0)
+        {
+            axisName.text = "Dpad left";
+            axisText.text = ControlInputWrapper.GetAxis(ControlInputWrapper.Axis.DPadX).ToString();
+        }
     }
 }
